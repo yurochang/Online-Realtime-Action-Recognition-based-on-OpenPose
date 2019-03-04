a580480a7d3db766eb8d1be69e6a306f0e08dac4
ChenYin <845139692@qq.com>
2019-03-04 21:28:33 +0900

    1. change the model read path, guarantee the smoothness of training and real-time test. 2. fix the parameter bug in train.py

--- Action/training/train.py
+++ Action/training/train.py
@@ -27,32 +27,32 @@ dummy_Y = np_utils.to_categorical(encoder_Y)
 X_train, X_test, Y_train, Y_test = train_test_split(X, dummy_Y, test_size=0.1, random_state=9)
 
 # build keras model
-# model = Sequential()
-# model.add(Dense(units=128, activation='relu'))
-# model.add(BatchNormalization())
-# model.add(Dense(units=64, activation='relu'))
-# model.add(BatchNormalization())
-# model.add(Dense(units=16, activation='relu'))
-# model.add(BatchNormalization())
-# model.add(Dense(units=4, activation='softmax'))
+model = Sequential()
+model.add(Dense(units=128, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(units=64, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(units=16, activation='relu'))
+model.add(BatchNormalization())
+model.add(Dense(units=4, activation='softmax'))
 
 # training
-# model.compile(loss='categorical_crossentropy', optimizer=Adam(0.0001), metrics=['accuracy'])
-# model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_test, Y_test))
-# model.save('framewise_recognition.h5')
+model.compile(loss='categorical_crossentropy', optimizer=Adam(0.0001), metrics=['accuracy'])
+model.fit(X_train, Y_train, batch_size=32, epochs=10, verbose=1, validation_data=(X_test, Y_test))
+model.save('framewise_recognition.h5')
 
 # test
-# model = load_model('framewise_recognition.h5')
+model = load_model('framewise_recognition.h5')
 
-# test_input = [0.43, 0.46, 0.43, 0.52, 0.4, 0.52, 0.39, 0.61, 0.4,
-#               0.67, 0.46, 0.52, 0.46, 0.61, 0.46, 0.67, 0.42, 0.67,
-#               0.42, 0.81, 0.43, 0.91, 0.45, 0.67, 0.45, 0.81, 0.45,
-#               0.91, 0.42, 0.44, 0.43, 0.44, 0.42, 0.46, 0.44, 0.46]
-# test_np = np.array(test_input)
-# test_np = test_np.reshape(-1, 36)
+test_input = [0.43, 0.46, 0.43, 0.52, 0.4, 0.52, 0.39, 0.61, 0.4,
+              0.67, 0.46, 0.52, 0.46, 0.61, 0.46, 0.67, 0.42, 0.67,
+              0.42, 0.81, 0.43, 0.91, 0.45, 0.67, 0.45, 0.81, 0.45,
+              0.91, 0.42, 0.44, 0.43, 0.44, 0.42, 0.46, 0.44, 0.46]
+test_np = np.array(test_input)
+test_np = test_np.reshape(-1, 36)
 
-# test_np = np.array(X[1033]).reshape(-1, 36)
-# if test_np.size > 0:
-#     pred = np.argmax(model.predict(test_np))
-#     init_label = encoder.inverse_transform(pred)
-#     print(init_label)
+test_np = np.array(X[1033]).reshape(-1, 36)
+if test_np.size > 0:
+    pred = np.argmax(model.predict(test_np))
+    init_label = encoder.inverse_transform([pred]) # the pred should be transformed to a list to be accepted
+    print(init_label)
